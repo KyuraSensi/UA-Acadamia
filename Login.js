8429f5dca74d7c36de83ba52995a1d863fa5e864
Utopianboy99 <123115444+Utopianboy99@users.noreply.github.com>
2023-06-15 13:47:29 +0200

    Login js (#15)
    * stoic's updated changes
    
    * First Modification
    
    * Js update
    
    * Js update (#6)
    
    * second commit (#7)
    
    * Kyu's final product
    
    ---------
    
    Co-authored-by: Tumelo <tthokoane02@gmail.com>
    Co-authored-by: KyuraSensi <thubelihlendlangamandla@gmail.com>

---
+++ Login.js
@@ -0,0 +1,67 @@
+
+
+
+// -------------------------Sign up page JavaScript------------------------------
+
+// Step 1 : Get all required information
+// Step 2 : Build Function
+// Step 3 : Add onClick functions to amy button
+
+
+// -------------Step 1 Get required information------------------
+
+let message = document.getElementById("message")
+let Passowrd = document.getElementById("pass1")
+let Password2 = document.getElementById("pass2")
+let button = document.getElementById("cornfim")
+
+// Step 2 : Build Function
+
+
+button.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    if (Passowrd.value == "" || Password2.value == "") {
+        alert("Please fill in all fields")
+    } else {
+        if (Passowrd.value == Password2.value || Password2.value == Passowrd.value) {
+            message.innerHTML = "Correct Password"
+            message.style.display = "block"
+            message.style.color = "green"
+            Passowrd.style.border = "3px solid green"
+            Password2.style.border = "3px solid green"
+
+            setTimeout(() => {
+                Passowrd.style.border = "1px solid black"
+                Password2.style.border = "1px solid black"
+                message.style.display = "none"
+            }, 2000)
+
+            Passowrd.value = ""
+            Password2.value = ""
+        }
+        if (Passowrd.value != Password2.value || Password2.value != Passowrd.value) {
+            message.innerHTML = "Password does not match"
+            message.style.display = "block"
+            message.style.color = "red"
+            Passowrd.style.border = "3px solid red"
+            Password2.style.border = "3px solid red"
+
+            setTimeout(() => {
+                Passowrd.style.border = "1px solid black"
+                Password2.style.border = "1px solid black"
+                message.style.display = "none"
+            }, 2000)
+
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
